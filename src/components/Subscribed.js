d53fb298fabb03d5159c3dde1780e055f718bd60
Yashin <yashinnaran@gmail.com>
2023-08-17 13:32:45 +0200

    Font styling and placement for Subcribe.js component has been added

--- src/components/Subscribed.js
+++ src/components/Subscribed.js
@@ -1,16 +1,20 @@
-import "./Subscribed.module.css";
+import styles from "./Subscribed.module.css";
+import IconList from "./IconList";
 
 const Subscribed = () => {
   return (
-    <section>
-      <div>
-        <span></span>
-        <h1>Thanks for subscribing</h1>
+    <section className={`${styles.subscribed_container}`}>
+      <div className={`${styles.content_wrapper}`}>
+        <span className={styles.tick_icon}>
+          <IconList width={64} height={64} />
+        </span>
+        <h1 className={`${styles.title}`}>Thanks for subscribing!</h1>
         <p>
-          A confirmation email has been sent to ash@loremcompany.com. Please
-          open it and click the button inside to confirm your subscription.
+          A confirmation email has been sent to{" "}
+          <strong>ash@loremcompany.com.</strong> Please open it and click the
+          button inside to confirm your subscription.
         </p>
-        <button>Dismiss message</button>
+        <button className={`${styles.dismiss_btn}`}>Dismiss message</button>
       </div>
     </section>
   );
