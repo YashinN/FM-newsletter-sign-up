441293fa7d389bba77beef477a905c7447962105
Yashin <yashinnaran@gmail.com>
2023-08-17 14:17:28 +0200

    email of user is now passed to the Subscribed.js component via props

--- src/components/Subscribed.js
+++ src/components/Subscribed.js
@@ -1,7 +1,7 @@
 import styles from "./Subscribed.module.css";
 import IconList from "./IconList";
 
-const Subscribed = () => {
+const Subscribed = ({ email }) => {
   return (
     <section className={`${styles.subscribed_container}`}>
       <div className={`${styles.content_wrapper}`}>
@@ -10,9 +10,8 @@ const Subscribed = () => {
         </span>
         <h1 className={`${styles.title}`}>Thanks for subscribing!</h1>
         <p>
-          A confirmation email has been sent to{" "}
-          <strong>ash@loremcompany.com.</strong> Please open it and click the
-          button inside to confirm your subscription.
+          A confirmation email has been sent to <strong>{email}</strong> Please
+          open it and click the button inside to confirm your subscription.
         </p>
         <button className={`${styles.dismiss_btn}`}>Dismiss message</button>
       </div>
